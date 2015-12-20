e4f85b93ee1e42527f9d7289308947157f781190
pavol soucik <pavol.soucik@gmail.com>
2015-12-20 19:16:56 +0100

    first init

--- ExpressApp2/app.ts
+++ ExpressApp2/app.ts
@@ -28,7 +28,7 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/about', routes.about);
 app.get('/contact', routes.contact);
-
+app.get('/contactos', routes.contactos);
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
--- ExpressApp2/routes/index.ts
+++ ExpressApp2/routes/index.ts
@@ -14,3 +14,7 @@ export function about(req: express.Request, res: express.Response) {
 export function contact(req: express.Request, res: express.Response) {
     res.render('contact', { title: 'Contact', year: new Date().getFullYear(), message: 'Your contact page' });
 };
+
+export function contactos(req: express.Request, res: express.Response) {
+    res.render('contactos', { title: 'Contact', year: new Date().getFullYear(), message: 'Your contact page' });
+};
